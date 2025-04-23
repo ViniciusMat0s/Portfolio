926ae4aaf9386e1ac6b00d6d2f83c26ba9afad44
ViniciusMat0s <vinibmatos@rede.ulbra.br>
2025-04-23 13:06:13 -0300

    base

---
+++ script.js
@@ -0,0 +1,16 @@
+function enviarMensagem(event) {
+    event.preventDefault()
+
+    const nome = document.getElementById("nome").value;
+    const mensagem = document.getElementById("mensagem").value;
+    const telefone = "5551989544006"
+
+    const texto = `Olá! Me chamo ${nome}, ${mensagem}`
+    const msgFormatada = encodeURIComponent(texto)
+
+    const url = `https://wa.me/${telefone}?text=${msgFormatada}`;
+
+    console.log(url);
+
+    window.open(url, "_blank")
+}
